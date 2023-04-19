fe2cd62dc92578fdc58d4bf8f049bb6f99ca4ad1
nagisa <carrotsoft0925@outlook.com>
2023-04-19 21:33:01 +0300

    editing

---
+++ cmake-build-debug/CMakeFiles/Principles_of_C_Programming_Project_Work.dir/linked_list.c.obj.d
@@ -0,0 +1,25 @@
+CMakeFiles/Principles_of_C_Programming_Project_Work.dir/linked_list.c.obj: \
+ C:\Users\x109649\Downloads\CT60A2500-Principles_of_C-Programming-Project\linked_list.c \
+ C:/Users/x109649/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/x86_64-w64-mingw32/include/stdio.h \
+ C:/Users/x109649/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ C:/Users/x109649/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/x86_64-w64-mingw32/include/corecrt.h \
+ C:/Users/x109649/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/x86_64-w64-mingw32/include/_mingw.h \
+ C:/Users/x109649/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/Users/x109649/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/Users/x109649/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/x86_64-w64-mingw32/include/vadefs.h \
+ C:/Users/x109649/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/Users/x109649/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/Users/x109649/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/Users/x109649/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ C:/Users/x109649/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/x86_64-w64-mingw32/include/stdlib.h \
+ C:/Users/x109649/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/x86_64-w64-mingw32/include/corecrt_wstdlib.h \
+ C:/Users/x109649/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/lib/gcc/x86_64-w64-mingw32/11.2.0/include-fixed/limits.h \
+ C:/Users/x109649/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/lib/gcc/x86_64-w64-mingw32/11.2.0/include-fixed/syslimits.h \
+ C:/Users/x109649/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/x86_64-w64-mingw32/include/limits.h \
+ C:/Users/x109649/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/x86_64-w64-mingw32/include/crtdefs.h \
+ C:/Users/x109649/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/x86_64-w64-mingw32/include/sec_api/stdlib_s.h \
+ C:/Users/x109649/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/x86_64-w64-mingw32/include/malloc.h \
+ C:/Users/x109649/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/lib/gcc/x86_64-w64-mingw32/11.2.0/include/mm_malloc.h \
+ C:/Users/x109649/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/x86_64-w64-mingw32/include/errno.h \
+ C:/Users/x109649/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/x86_64-w64-mingw32/include/string.h \
+ C:/Users/x109649/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/x86_64-w64-mingw32/include/sec_api/string_s.h
--- linked_list.c
+++ linked_list.c
@@ -1,8 +1,215 @@
 //
 // Created by x109649 on 19.4.2023.
 //
-typedef struct node{
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct node {
     char data[10];
     struct node *pNext;
 } Node;
 
+int isEmpty(Node *head) {
+    if (head == NULL) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+Node *addToEnd(Node *head, char str[]) {
+    Node *pNew = (Node *) malloc(sizeof(Node));
+    Node *pHead = head;
+    strcpy(pNew->data, str);
+    pNew->pNext = NULL;
+
+    if (head == NULL) {
+        pHead = pNew;
+    } else {
+        while (head->pNext != NULL) {
+            head = head->pNext;
+        }
+        head->pNext = pNew;
+    }
+
+    return pHead;
+}
+
+Node *addToBeginning(Node *head, char str[]) {
+    Node *pNew = (Node *) malloc(sizeof(Node));
+    strcpy(pNew->data, str);
+    pNew->pNext = NULL;
+
+    if (head == NULL) {
+        head = pNew;
+    } else {
+        pNew->pNext = head;
+        head = pNew;
+    }
+
+    return head;
+}
+
+int size(Node *head) {
+    int n = 0;
+    while (head->pNext != NULL) {
+        n++;
+        head = head->pNext;
+    }
+
+    return n;
+}
+
+Node *tail(Node *head) {
+    while (head->pNext != NULL) {
+        head = head->pNext;
+    }
+
+    return head;
+}
+
+Node *get_node(Node *head, int pos) {
+    int n = size(head);
+
+    if (pos > n) {
+        return NULL;
+    }
+
+    int i = 1;
+    while (head->pNext != NULL) {
+        if (i == pos) {
+            return head;
+        }
+        head = head->pNext;
+    }
+
+    return head; //never use
+}
+
+Node *deleteFirst(Node *head) {
+    if (isEmpty(head)) {
+        return NULL;
+    }
+
+    if (head->pNext == NULL) {
+        free(head);
+        return NULL;
+    }
+
+    Node *pNewFirst = head;
+    free(head);
+
+    return pNewFirst;
+}
+
+Node *deleteLast(Node *head) {
+    if (isEmpty(head)) {
+        return NULL;
+    }
+
+    if (head->pNext == NULL) {
+        free(head);
+        return NULL;
+    }
+
+    Node *pPrev, *pFirst = head;
+    while (head->pNext != NULL) {
+        pPrev = head;
+        head = head->pNext;
+    }
+
+    free(head);
+    pPrev->pNext = NULL;
+    return pFirst;
+}
+
+int find(Node *head, char *str) {
+    if (isEmpty(head)) {
+        return 0;
+    }
+
+    if (head->data == str) {
+        return 1;
+    }
+
+    int i = 1;
+    while (head->pNext != NULL) {
+        head = head->pNext;
+        i++;
+        if (head->data == str) {
+            return i;
+        }
+    }
+
+    return 0;
+}
+
+void print(Node *head) {
+    if (isEmpty(head)) {
+        printf("List is empty.\n");
+        return;
+    }
+
+    int i = 1;
+    printf("Printing...\n");
+    printf("Node position 1: %s.\n", head->data);
+
+    while (head->pNext != NULL) {
+        i++;
+        head = head->pNext;
+        printf("Node position %d: %s.\n", i, head->data);
+    }
+}
+
+char *toString(Node *head) {
+    if (isEmpty(head)) {
+        return "List is empty.\n";
+    }
+
+    char *ans = strcat("(", head->data);
+    ans = strcat(ans, ")");
+
+    while (head->pNext != NULL) {
+        head = head->pNext;
+        ans = strcat(ans, " -> (");
+        ans = strcat(ans, head->data);
+        ans = strcat(ans, ")");
+    }
+
+    return ans;
+}
+
+Node *reverse(Node *head) {
+    if (isEmpty(head)) {
+        return NULL;
+    }
+
+    if (head->pNext == NULL) {
+        return NULL;
+    }
+
+    Node *pPrev, *pNext = head->pNext;
+    head->pNext = NULL;
+
+    while (1) {
+        pPrev = head;
+        head = pNext;
+//        pPrev->pNext = NULL;
+
+        if (head->pNext == NULL) {
+            head->pNext = pPrev;
+            return head;
+        }
+
+        pNext = head->pNext;
+
+        head->pNext = pPrev;
+
+//        pPrev = head;
+//        head = pNext;
+//        pNext = pNext->pNext;
+    }
+
+//    return head;
+}
